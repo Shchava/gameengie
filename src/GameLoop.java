ba6b3bd66b48e16a44e57dbb78eed8609941effe
Yaroslav-Petro_Shchavinskyi <yarosshc@gmail.com>
2021-03-14 20:24:53 +0200

    synchronize app in one thread

--- src/GameLoop.java
+++ src/GameLoop.java
@@ -1,7 +1,7 @@
 import javafx.application.Platform;
 
 public class GameLoop implements Runnable {
-
+    final public static NotReentrantLock PROCESS = new NotReentrantLock();
 
     @Override
     public void run() {
@@ -13,29 +13,36 @@ public class GameLoop implements Runnable {
 
             //	Display.drawFrameOnImage();
             try {
+
+                PROCESS.lock();
                 Platform.runLater(() -> {
                     Display.drawTexturedFrame();
-                });
+//                    Display.drawFrameOnImage();
 
+                });
             } catch (Exception ex) {
                 System.out.println(ex);
+                PROCESS.unlock();
             }
+
             fps++;
             if (System.currentTimeMillis() > time) {
                 final int fp = fps;
                 Platform.runLater(() ->
                 {
+                    System.out.println(fp);
                     Display.setFps(fp);
                 });
                 fps = 0;
                 time = System.currentTimeMillis() + 1000;
             }
 
-            try {
-                Thread.sleep(60); //50 for 700x500
-            } catch (InterruptedException ex) {
 
-            }
+//            try {
+//                Thread.sleep(50); //50 for 700x500
+//            } catch (InterruptedException ex) {
+//
+//            }
         }
 
     }
